b6fa1d56ab911df5f68aaff4955e2e8da6259679
kija56 <kijamabula2@gmail.com>
2020-02-16 09:11:41 +0300

    Working on relations for employee and company models

--- app/Company.php
+++ app/Company.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    protected $fillable = array('name', 'email','website', 'price','logo');
+
+    public function employees(){
+        return $this->hasMany(Employee::class);
+    }
 }
--- app/Employee.php
+++ app/Employee.php
@@ -5,6 +5,11 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
-{
-    //
+{   
+
+    protected $fillable = array('firstName','lastName', 'email','company','phone');
+
+    public function company(){
+        return $this->belongsTo(Company::class);
+    }
 }
