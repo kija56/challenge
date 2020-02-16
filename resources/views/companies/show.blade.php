1ebe135de22f504ed4ee5965dfddb5a5261c5ee0
kija56 <kijamabula2@gmail.com>
2020-02-16 08:55:39 +0300

    Working on showing details of a single company

--- resources/views/companies/show.blade.php
+++ resources/views/companies/show.blade.php
@@ -1,4 +1,28 @@
 @extends('layouts.app')
-@section('content')
 
+@section('content')
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="col-md-3 p-3">
+            <img class="mr-3 rounded-circle align-self-center img-fluid w-100" src="/storage/{{$company->logo}}" alt="">
+        </div>
+        <div class="col-md-9 pt-3">
+            <div class="d-flex justify-content-between align-items-baseline">
+                <div class="d-flex align-items-center pb-3">
+                    <div class="h4">{{$company->name}}</div>
+
+                </div>
+                <div class="pt-4 font-weight-bold">Email: {{$company->email}}</div>
+                <div class=""><b>Website:</b> {{$company->website}}</div>
+            </div>
+            <div class="d-flex">
+
+            </div>
+            
+        </div>
+    </div>
+    <div class="row  pt-3">
+        
+    </div>
+</div>
 @endsection
