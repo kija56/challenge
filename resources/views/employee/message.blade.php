b8921ebd8f9a416588c0298c547843a40bae5f6a
kija56 <kijamabula2@gmail.com>
2020-02-20 15:33:20 +0300

    Add compose email page

---
+++ resources/views/employee/message.blade.php
@@ -0,0 +1,43 @@
+<!-- Main content -->
+<section class="content">
+    <div class="row">
+        <!-- /.col -->
+        <div class="col-md-9">
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Compose New Message</h3>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                    <div class="form-group">
+                        <input class="form-control" placeholder="To:">
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" placeholder="Subject:">
+                    </div>
+                    <div class="form-group">
+                        <textarea id="compose-textarea" class="form-control" style="height: 300px">
+                      <h1><u>Heading Of Message</u></h1>
+                      <h4>Subheading</h4>
+                      <p></p>
+                      <p>Thank you,</p>
+                      <p>John Doe</p>
+                    </textarea>
+                    </div>
+                </div>
+                <!-- /.box-body -->
+                <div class="box-footer">
+                    <div class="pull-right">
+                        <button type="submit" class="btn btn-primary"><i class="fa fa-envelope-o"></i> Send</button>
+                    </div>
+                    <button type="reset" class="btn btn-default"><i class="fa fa-times"></i> Discard</button>
+                </div>
+                <!-- /.box-footer -->
+            </div>
+            <!-- /. box -->
+        </div>
+        <!-- /.col -->
+    </div>
+    <!-- /.row -->
+</section>
+<!-- /.content -->
