a5ef5695167644508a1c7807d9158435958c6437
kija56 <kijamabula2@gmail.com>
2020-02-20 14:03:48 +0300

    Edit the system home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,34 +2,24 @@
 
 @section('content')
 <div class='row'>
-    <div class="col-lg-12"><p><i>{{ trans('sentence.welcome')}}</i> <b>{{ Auth::user()->name }}</b></p></div>
-    <div class="col-lg-6">
+    <div class="col-lg-1">
+        <p></b></p>
+    </div>
+    <div class="col-lg-10">
         <div class="card card-primary card-outline">
             <div class="card-header">
-                <h5 class="m-0">Featured</h5>
+                <h5 class="m-0 text-align-center"><i>{{ trans('sentence.welcome')}}</i> <b>{{ Auth::user()->name }}</h5>
             </div>
             <div class="card-body">
-                <h6 class="card-title">Special title treatment</h6>
+                <h6 class="card-title">You have access to the following:</h6>
+                <ul>
+                    <li class="card-text">Add, view, edit and delete a company</li>
+                    <li class="card-text">Add, view, edit, and delete an employee</li>
+                    <li class="card-text">Send email to employees</li>
+                </ul>
 
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
         </div>
     </div>
-    <!-- /.col-md-6 -->
-    <div class="col-lg-6">
-        <div class="card card-primary card-outline">
-            <div class="card-header">
-                <h5 class="m-0">Featured</h5>
-            </div>
-            <div class="card-body">
-                <h6 class="card-title">Special title treatment</h6>
-
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-    </div>
-    <!-- /.col-md-6 -->
 </div><!-- /.row -->
 @endsection
