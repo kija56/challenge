a8771980326194caf7bbce03dfddc46edb8d684a
kija56 <kijamabula2@gmail.com>
2020-02-20 20:46:53 +0300

    Add action buttons in the companies table

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -31,7 +31,15 @@
                                 <td>{{$company->email}}
                                 </td>
                                 <td>{{$company->website}}</td>
-                                <td></td>
+                                <td class="d-flex">
+                                    <div ><a class="btn btn-info btn-sm" href="/companies/{{$company->id}}"><i class="fa fa-eye"></i> </a></div>
+                                    <div class="pr-2 pl-2"><a class= "btn btn-primary btn-sm" href="/companies/{{$company->id}}/edit"><i class="fa fa-edit"></i></a></div>
+                                    <form action="/companies/{{$company->id}}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                        <button type="submit" class= "btn btn-danger btn-sm" ><i class="fa fa-trash"></i></button>
+                                    </form>
+                                 </td>
                                 
                             </tr>
                             @endforeach
