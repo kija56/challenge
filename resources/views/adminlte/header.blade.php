bbc0f01a5ea60252adb3163834ca68a6563050f8
kija56 <kijamabula2@gmail.com>
2020-02-20 12:16:51 +0300

    Add language folders making the system multilanguage

--- resources/views/adminlte/header.blade.php
+++ resources/views/adminlte/header.blade.php
@@ -24,6 +24,26 @@
   </ul>
   <!-- Right navbar links -->
   <ul class="navbar-nav ml-auto">
+    <!-- Language Dropdown Menu -->
+    <li class="nav-item dropdown">
+      <a class="nav-link" data-toggle="dropdown" href="#">
+        <i class="flag-icon flag-icon-us"></i>
+      </a>
+      <div class="dropdown-menu dropdown-menu-right p-0">
+        <a href="#" class="dropdown-item active">
+          <i class="flag-icon flag-icon-us mr-2"></i> English
+        </a>
+        <a href="#" class="dropdown-item">
+          <i class="flag-icon flag-icon-de mr-2"></i> German
+        </a>
+        <a href="#" class="dropdown-item">
+          <i class="flag-icon flag-icon-fr mr-2"></i> French
+        </a>
+        <a href="#" class="dropdown-item">
+          <i class="flag-icon flag-icon-es mr-2"></i> Spanish
+        </a>
+      </div>
+    </li>
     <li class="nav-item dropdown">
       <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
         {{ Auth::user()->name }} <span class="caret"></span>
--- resources/views/adminlte/index.blade.php
+++ resources/views/adminlte/index.blade.php
@@ -3,14 +3,15 @@
 This is a starter template page. Use this page to start your new project from
 scratch. This page gets rid of all links and provides the needed markup only.
 -->
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-  <meta charset="utf-8">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
   <title>{{ config('app.name', 'Challenge') }}</title>
-
+ <!-- flag-icon-css -->
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.3.0/css/flag-icon.min.css">
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="{{ asset('/bower_components/admin-lte/plugins/fontawesome-free/css/all.min.css')}}">
   <!-- Theme style -->
